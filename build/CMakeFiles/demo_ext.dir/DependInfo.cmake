027d0291435b4d3122c3ce780488aba70f9b1ce6
Jiachao Deng <jiachao.deng@motorolasolutions.com>
2021-12-12 23:16:27 -0800

    Added title.

---
+++ build/CMakeFiles/demo_ext.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/deng/project/ruby-extension-process-safety/demo_ext.cpp" "/home/deng/project/ruby-extension-process-safety/build/CMakeFiles/demo_ext.dir/demo_ext.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "demo_ext_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/deng/.rbenv/versions/2.6.0/include/ruby-2.6.0"
+  "/home/deng/.rbenv/versions/2.6.0/include/ruby-2.6.0/x86_64-linux"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
